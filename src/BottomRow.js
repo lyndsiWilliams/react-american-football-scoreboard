d2cb47b27b56f461967be4d017ccf6a316769a0e
lyndsiWilliams <kcatgirl@gmail.com>
2019-11-04 16:40:39 -0600

    Added quarter functionality: Button goes up to 4 then stops, plus reset button

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [quarter, setQuarter] = useState(0);
+  
+  if(quarter > 4) {
+    setQuarter(4);
+  }
+
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,7 +24,11 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{quarter}</div>
+      </div>
+      <div className="quarterButtons">
+        <button onClick={ () => setQuarter(quarter + 1)} className="quarter__set">Quarter</button>
+        <button onClick={ () => setQuarter(0)} className="quarter__set">Reset</button>
       </div>
     </div>
   );
